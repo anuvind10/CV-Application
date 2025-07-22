d2a02f84e132b81e9337e3b5a2cc5dd6e6b88284
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-07-22 15:45:37 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BasicInfo from "../components/basic_info";
+
+function App() {
+  return (
+    <>
+      <BasicInfo></BasicInfo>
+    </>
+  );
+}
+
+export default App;
