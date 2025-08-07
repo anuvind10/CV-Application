0c6832dbad1ac07d63adc923b6a404c73cc2f689
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-08-07 10:28:24 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
 import "./App.css";
-import PersonalInfo from "../components/PersonalInfo";
-import Education from "../components/Education";
-import Experience from "../components/Experience";
+import PersonalInfo from "./components/PersonalInfo";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
 import expandIcon from "./images/expand.svg";
 // import collapse from "./images/collapse.svg";
 
 function App() {
   return (
-    <div className="input-section">
-      <section className="personalInfo">
-        <div className="header">
-          <h2>Personal Details</h2>
-          <img src={expandIcon} className="toggle-icon" id="personal-toggle" />
-        </div>
-        <PersonalInfo></PersonalInfo>
-      </section>
-      <section className="experience">
-        <div className="header">
-          <h2>Experience</h2>
-          <img
-            src={expandIcon}
-            className="toggle-icon"
-            id="experience-toggle"
-          />
-        </div>
-        <Experience></Experience>
-      </section>
-      <section className="education">
-        <div className="header">
-          <h2>Education</h2>
-          <img src={expandIcon} className="toggle-icon" id="education-toggle" />
-        </div>
-        <Education></Education>
-      </section>
-    </div>
+    <>
+      <div className="input-section">
+        <section className="personalInfo">
+          <div className="header">
+            <h2>Personal Details</h2>
+            <img
+              src={expandIcon}
+              className="toggle-icon"
+              id="personal-toggle"
+            />
+          </div>
+          <PersonalInfo></PersonalInfo>
+        </section>
+        <section className="experience">
+          <div className="header">
+            <h2>Experience</h2>
+            <img
+              src={expandIcon}
+              className="toggle-icon"
+              id="experience-toggle"
+            />
+          </div>
+          <Experience></Experience>
+        </section>
+        <section className="education">
+          <div className="header">
+            <h2>Education</h2>
+            <img
+              src={expandIcon}
+              className="toggle-icon"
+              id="education-toggle"
+            />
+          </div>
+          <Education></Education>
+        </section>
+      </div>
+      <div className="CV-preview">
+        <div className="CV-header"></div>
+        <div className="CV-content"></div>
+      </div>
+    </>
   );
 }
 
