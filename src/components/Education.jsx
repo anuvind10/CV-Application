901d5946e443f6e18e00a5351599e055d864cbe0
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-08-08 17:12:25 +0530

    ability to add and remove education details

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,23 +1,50 @@
-// import { useState } from "react";
+import { useState } from "react";
+import removeIcon from "../images/remove.svg";
 
 function Education() {
+  const [educations, setEducation] = useState([{ id: Date.now() }]);
+
+  function addEducation() {
+    setEducation((prev) => {
+      return [...prev, { id: Date.now() + Math.random() }];
+    });
+  }
+
+  function removeEducation(idToRemove) {
+    setEducation((prev) => prev.filter((edu) => edu.id !== idToRemove));
+  }
+
   return (
     <>
-      <div className="school-info">
-        <div>
-          <label htmlFor="schoolName">School Name:</label>
-          <input type="text" name="schoolName" />
+      {educations.map((edu) => (
+        <div key={`education-${edu.id}`} className="school-info">
+          <label htmlFor={`name-${edu.id}`}>School Name:</label>
+          <input type="text" name={`name-${edu.id}`} id={`name-${edu.id}`} />
+          <label htmlFor={`fromDate-${edu.id}`}>From</label>
+          <input
+            type="date"
+            name={`fromDate-${edu.id}`}
+            id={`fromDate-${edu.id}`}
+            className="date"
+          />
+          <label htmlFor={`toDate-${edu.id}`}>To</label>
+          <input
+            type="date"
+            name={`toDate-${edu.id}`}
+            id={`toDate-${edu.id}`}
+            className="date"
+          />
+          <img
+            src={removeIcon}
+            alt="remove education"
+            className="remove-icon"
+            onClick={() => {
+              removeEducation(edu.id);
+            }}
+          />
         </div>
-        <div>
-          <label htmlFor="fromDate">From</label>
-          <input type="date" name="fromDate" id="fromDate" className="date" />
-        </div>
-        <div>
-          <label htmlFor="toDate">To</label>
-          <input type="date" name="toDate" id="toDate" className="date" />
-        </div>
-      </div>
-      <button>Add Education</button>
+      ))}
+      <button onClick={addEducation}>Add Education</button>
     </>
   );
 }
