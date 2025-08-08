55799f1c3a4385714d6b6d0c440fef05caa4bd77
Anuvind Ashok <anuvind.ashok10@gmail.com>
2025-08-08 18:34:04 +0530

    collapsable sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import PersonalInfo from "./components/PersonalInfo";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import expandIcon from "./images/expand.svg";
+import { useState } from "react";
 // import collapse from "./images/collapse.svg";
 
-function Section({ className, header, section }) {
+function Section({ className, header, section, isActive, onActivate }) {
   return (
     <>
       <section className={className}>
-        <div className="header">
+        <div className="header" onClick={() => onActivate(className)}>
           <h2>{header}</h2>
-          <img src={expandIcon} alt="expand section" className="toggle-icon" />
+          <img
+            src={expandIcon}
+            alt="expand section"
+            className={`toggle-icon ${isActive ? "expanded" : "collapsed"}`}
+          />
         </div>
-        {section}
+        <div
+          className={`section-content ${isActive ? "expanded" : "collapsed"}`}
+        >
+          {section}
+        </div>
       </section>
     </>
   );
 }
 
 function App() {
+  const [activeSection, setActiveSection] = useState("personal-info");
+
   return (
     <>
       <div className="input-section">
@@ -27,16 +38,22 @@ function App() {
           className="personal-info"
           header="Personal Details"
           section={<PersonalInfo></PersonalInfo>}
+          isActive={activeSection === "personal-info"}
+          onActivate={setActiveSection}
         ></Section>
         <Section
           className="experience"
           header="Experience"
           section={<Experience></Experience>}
+          isActive={activeSection === "experience"}
+          onActivate={setActiveSection}
         ></Section>
         <Section
           className="education"
           header="Education"
           section={<Education></Education>}
+          isActive={activeSection === "education"}
+          onActivate={setActiveSection}
         ></Section>
       </div>
       <div className="CV-preview">
